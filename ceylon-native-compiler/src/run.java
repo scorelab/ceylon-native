5457aa9d38f7e30398f4eb9468b086982c59e1d4
tharindupr <tharindu.prf@gmail.com>
2016-08-11 10:08:33 +0530

    change project to netbeasn

---
+++ ceylon-native-compiler/src/run.java
@@ -0,0 +1,9 @@
+
+import com.redhat.ceylon.compiler.java.runtime.tools.*;
+public class run {
+	
+	public static void main(String[] args) {
+        System.out.println("Hello Wold");
+    }
+
+}
--- ceylon-native/src/ceylon/pkgnative/run.java
+++ ceylon-native/src/ceylon/pkgnative/run.java
@@ -9,6 +9,27 @@ package ceylon.pkgnative;
  *
  * @author tharindu
  */
+import com.redhat.ceylon.compiler.java.runtime.tools.*;
+import java.io.File;
+
 public class run {
-    
+    public static void main(String[] args){
+        CompilerOptions options = new CompilerOptions();
+        options.addModule("com.example");
+
+        CompilationListener listener = new CompilationListener(){
+            @Override
+            public void error(File file, long line, long column, String message){}
+            @Override
+            public void warning(File file, long line, long column, String message){}
+            @Override
+            public void moduleCompiled(String module, String version){}
+        };
+
+        Compiler jvmCompiler = CeylonToolProvider.getCompiler(Backend.Java);
+        jvmCompiler.compile(options, listener);
+
+        Compiler jsCompiler = CeylonToolProvider.getCompiler(Backend.JavaScript);
+        jsCompiler.compile(options, listener);
+    }
 }
