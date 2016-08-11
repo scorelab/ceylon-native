1f4b7e78d83bac4da98c6e705f41a28de392bf72
tharindupr <tharindu.prf@gmail.com>
2016-08-11 21:14:09 +0530

    Ceylon module running from Java

--- samplepograms-java/Factorial.java
+++ samplepograms-java/Factorial.java
@@ -1,10 +1,11 @@
+package abc;
 public class Factorial
 {
-	public static void main(String[] args)
+	/*public static void main(String[] args)
 	{	final int NUM_FACTS = 100;
 		for(int i = 0; i < NUM_FACTS; i++)
 			System.out.println( i + "! is " + factorial(i));
-	}
+	}*/
 	
 	public static int factorial(int n)
 	{	int result = 1;
--- samplepograms-java/HelloWorld.java
+++ samplepograms-java/HelloWorld.java
@@ -1,9 +1,9 @@
-/* HelloWorld.java
- */
-
+//package abc;
+//import Factorial;
 public class HelloWorld
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		//Factorial avd=new Factorial();
 	}
 }
