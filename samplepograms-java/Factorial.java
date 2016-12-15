ae52d6edb409923815a7b8a7e77057e93b1f6e41
Jagreet Das Gupta <jagreetdg@gmail.com>
2016-12-15 12:49:54 +0530

    Optimzed Factorial program..
    Implemented Dynamic Programming

--- samplepograms-java/Factorial.java
+++ samplepograms-java/Factorial.java
@@ -1,16 +1,23 @@
 package abc;
-public class Factorial
-{
-	/*public static void main(String[] args)
-	{	final int NUM_FACTS = 100;
-		for(int i = 0; i < NUM_FACTS; i++)
-			System.out.println( i + "! is " + factorial(i));
-	}*/
-	
-	public static int factorial(int n)
-	{	int result = 1;
-		for(int i = 2; i <= n; i++)
-			result *= i;
+import java.util.*;
+import java.math.*;
+public class Factorial {
+	/*
+	 *public static void main(String[] args) { final int NUM_FACTS = 100;
+	 *for(int i = 0; i < NUM_FACTS; i++) System.out.println( i + "! is " +
+	 *factorial(i)); }
+	 */
+	//Using Dynamic Programming...
+    static Map<Integer,BigInteger> map = new HashMap<Integer,BigInteger>();
+    static{
+    	map.put(1,BigInteger.ONE);
+    	map.put(0, BigInteger.ZERO);
+    }
+    //BigInteger is used to accomodate factorials of large numbers
+	public static BigInteger factorial(int n) {
+		if(map.containsKey(n)) return map.get(n);
+		BigInteger result = factorial(n-1).multiply(BigInteger.valueOf(n));
+		map.put(n, result);
 		return result;
 	}
 }
